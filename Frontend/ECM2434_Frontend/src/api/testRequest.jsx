f9afe17502a096eb55a934bf23012d7636f7c27c
Jeremy Shorter <jeremy.shorter@hatless.studio>
2024-02-21 13:21:24 +0000

    Added some api stuff to make it easier for future implementations.

--- Frontend/ECM2434_Frontend/src/api/testRequest.jsx
+++ Frontend/ECM2434_Frontend/src/api/testRequest.jsx
@@ -1,15 +1,42 @@
-import axios from "axios";
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
 
-export default function testRequest(setResponse) {
-  axios
-    .get("http://localhost:8000/api/test_api/")
-    // For HTTPS .get("https://localhost:8000/api/test_api/")
-    .then((response) => {
-      console.log(response);
-      setResponse(response.data.message);
-    })
-    .catch((error) => {
-      console.log(error);
-      setResponse(error.message);
+
+const baseURL = "http://localhost:8000/api/";
+// Currently set to HTTP. Could use a config file to modify this easily. 
+
+export default class ApiClient {
+  constructor() {
+    this.axios = axios.create({
+      baseURL,
+      withCredentials: true,
     });
+  }
+
+  async testRequest() {
+    axios.get("/test_api/")
+      .then((response) => {
+        console.log(response);
+        return response.data.message;
+      })
+      .catch((error) => {
+        console.log(error);
+        return error.message;
+      });
+  }
+
+  async get(url, config) {
+    const response = await this.axios.get(url, config);
+    return response.data;
+  }
+
+  async post(url, data, config) {
+    const response = await this.axios.post(url, data, config);
+    return response.data;
+  }
+
+  async login(email, password) {
+    const response = await this.post("/v1/users/login/", { email, password });
+    return response;
+  }
 }
+
