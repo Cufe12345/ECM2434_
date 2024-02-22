52eb5039f424c0cfbb95d388bac6a3005cfcd2c7
Stickman230 <123313783+Stickman230@users.noreply.github.com>
2024-02-22 02:09:01 +0000

    Update urls.py
    - corrected mistake

--- Backend/myapi/urls.py
+++ Backend/myapi/urls.py
@@ -7,9 +7,9 @@ from .views import TestAPIView
 urlpatterns = [
     path('test_api/', TestAPIView.as_view(), name='test_api'),
     path('quest/',views.getQuest, name='Quest'),
-    path('quest/add',views.addQuest, name='Add quest'),
+    path('quest/add/',views.addQuest, name='Add quest'),
     path('user/', views.getUser, name ="user"),
-    path('user/add', views.addUser, name="add user"),
+    path('user/add/', views.addUser, name="add user"),
     path('account/',include('djoser.urls')),
     path('account/',include('djoser.urls.authtoken')),
 ]
