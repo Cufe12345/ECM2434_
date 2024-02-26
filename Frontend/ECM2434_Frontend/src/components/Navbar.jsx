3e4de2d3f5a5eb23bae6645b63f48d4e4606652e
GKA2003 <126076201+GKA2003@users.noreply.github.com>
2024-02-26 00:48:30 +0000

    Landing (Home) page UI

--- Frontend/ECM2434_Frontend/src/components/Navbar.jsx
+++ Frontend/ECM2434_Frontend/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import { NavLink } from "react-router-dom";
+import logoImage from '../assets/images/logo.png';
 
 const Navbar = () => {
     return (
         <div className="banner">
             <div className="navContainer">
-                <h1>Logo</h1>
+                <img src={logoImage} alt="Logo" className="navbarLogo" />
                 <div className="navLinks">
                     <NavLink to="/">Home</NavLink>
                     <NavLink to="/Login">Login</NavLink>
--- Frontend/ECM2434_Frontend/src/pages/Home.jsx
+++ Frontend/ECM2434_Frontend/src/pages/Home.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useUser } from '../contexts/userContext';
 import {CreateQuestForm} from '../components/forms/createQuest/createQuestForm';
 import classes from './Home.module.css';
+
 const Home = () => {
     
     //This is how you use the user context, you can access all these variables and functions from the context
     const { user,setUser,userData,userDataError,userDataLoading} = useUser();
-    
+    const [showForm, setShowForm] = useState(false);
     //ctrl i
 
     return (
@@ -15,8 +16,15 @@ const Home = () => {
             <h1>Loading...</h1>
             : 
             <div className={classes.container}>
-                <h1>Home</h1>
-                <CreateQuestForm/>
+                <div className={classes.leftSideContent}>
+                    <h1>Daily <span className={classes.ecoGradient}>Eco</span> Quest</h1>
+                    <div className={classes.textGroup}>
+                     <h5>Challenge your friends at Uni,</h5>
+                     <h5>Save the planet.</h5>
+                    </div>
+                    {!showForm && <button onClick={() => setShowForm(true)}>Create Quest</button>}
+                </div>
+                {showForm && <CreateQuestForm />}
             </div>
     );
 }
