17198ceee1b52117b6385b6f25b17b869ae88e29
Stickman230 <123313783+Stickman230@users.noreply.github.com>
2024-02-28 15:49:06 +0000

    Add files via upload
    added comments and authors

---
+++ Backend/SSL/README.md
@@ -0,0 +1,7 @@
+## Use the SSL folder if HTTPS is needed 
+
+# there should be all needed resources to run the server with a SSL certificate on HTTPS 
+
+- cert.pem
+
+- key.pem
---
+++ Backend/myapi/apps.py
@@ -0,0 +1,8 @@
+'''
+API app configuration
+'''
+from django.apps import AppConfig
+
+class MyapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myapi'
