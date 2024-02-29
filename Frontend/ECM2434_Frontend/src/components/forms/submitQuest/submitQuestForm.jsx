e4bacf8d3009c8f326ce4a61ceaa7e780bc38b5e
Cufe12345 <73361589+Cufe12345@users.noreply.github.com>
2024-02-29 11:20:08 +0000

    enabled create quest component as need it there for demo

--- Frontend/ECM2434_Frontend/src/components/forms/submitQuest/submitQuestForm.jsx
+++ Frontend/ECM2434_Frontend/src/components/forms/submitQuest/submitQuestForm.jsx
@@ -4,7 +4,7 @@ import { useUser } from "../../../contexts/userContext";
 import { ImageSubmit } from "../../imageSubmit";
 
 //Created by Cufe12345(Callum Young)
-export function SubmitQuestForm({onBackClick,setOpen}) {
+export function SubmitQuestForm({ onBackClick, setOpen }) {
   const { user, userData } = useUser();
 
   //Stores the file
@@ -13,24 +13,40 @@ export function SubmitQuestForm({onBackClick,setOpen}) {
   /**
    * This function submits the quest
    */
-  function submitQuest() {
+  async function submitQuest() {
     console.log("Submitting quest");
-    //submit on backend and if successful
+    let dataImg = {
+      name: file.name,
+      description: "n/a for now",
+    };
+    let imgURL = null;
+    await ApiClient.api
+      .uploadImage(user, dataImg, file)
+      .then((res) => {
+        imgURL = res?.image;
+        console.log(res);
+      })
+      .catch((error) => {
+        console.warn(error);
+      });
+    //submit on backend and pass imgURL and if successful
     setOpen(true);
     //Redirect to Feed
   }
 
-
-
   return (
     <div className={classes.card}>
       <div className={classes.backContainer}>
-        <button onClick={onBackClick} className={classes.backButton}><p>Back</p></button>
+        <button onClick={onBackClick} className={classes.backButton}>
+          <p>Back</p>
+        </button>
       </div>
       <h1>Submit Quest</h1>
       <p>Attach an image of completed quest below</p>
-      <ImageSubmit setImage={setFile} img={file}/>
-      <button onClick={submitQuest} className={classes.submitButton}>Submit</button>
+      <ImageSubmit setImage={setFile} img={file} />
+      <button onClick={submitQuest} className={classes.submitButton}>
+        Submit
+      </button>
     </div>
   );
 }
